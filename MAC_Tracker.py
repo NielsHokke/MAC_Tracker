2521a7ae7ff7231cc71bd4642c8dcf3ad5fb756f
Niels Hokke <nielshokke@gmail.com>
2018-03-01 14:03:39 +0100

    removed print

--- MAC_Tracker.py
+++ MAC_Tracker.py
@@ -6,7 +6,7 @@ from matplotlib.dates import DayLocator, HourLocator, DateFormatter, drange
 import struct
 import numpy as np
 
-file_path = 'dinsdagwoensdag.csv'
+file_path = 'measurments/dinsdagwoensdagfixed.csv'
 
 # 1519302005.708943170	ac:bc:32:c6:da:59	-52
 
@@ -90,7 +90,6 @@ def show_uniqueMacoverTime():
 			lastTime = float(time_stamp)
 
 		if float(time_stamp) > lastTime + timeslot:
-			# print("{} > {} + {}".format(float(time_stamp), lastTime, timeslot))
 			y.append(len(Mac_list))
 			x.append(dt.datetime.fromtimestamp(int(float(time_stamp))))
 			Mac_list = []
@@ -103,16 +102,16 @@ def show_uniqueMacoverTime():
 
 	fig, ax = plt.subplots()
 
-	plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d %H:%M:%S'))
+	plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%a %d %H:%M:%S'))
 	ax.set_xlim([min(x) - dt.timedelta(hours=1), max(x) + dt.timedelta(hours=1)])
 	plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=2))
 	plt.gca().xaxis.set_minor_locator(mdates.MinuteLocator(interval=15))
 	plt.gcf().autofmt_xdate()
 
-	# plt.bar(x, y, width=0.03, align='center')
-	plt.plot(x, y)
+	plt.bar(x, y, width=0.025)  # , align='center'
+	# plt.plot(x, y)
 	plt.show()
 
 
-show_MACoverTime()
-# show_uniqueMacoverTime()
+# show_MACoverTime()
+show_uniqueMacoverTime()
