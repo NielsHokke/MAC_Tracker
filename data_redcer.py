ee49a2c4ec5cc1d3c01ae952e18f29ac6f97f444
jetsebrouwer <jetse.brouwer@gmail.com>
2018-03-01 14:01:21 +0100

    added whitelist capabilities

--- data_redcer.py
+++ data_redcer.py
@@ -2,10 +2,16 @@ from tkinter import ttk
 import tkinter
 import csv
 import collections
+import datetime
 
-declutter_min = 3600.0
+declutter_min = 0.0
 declutter_max = 300000.0
 Ignore_Singletons = True
+starts_with = "94:65:2d:2d:14:17"
+
+# Mark's Mac "c0:ee:fb:92:d6:01"
+# Jetse's Mac "34:80:b3:f0:30:69"
+# Niels Mac "94:65:2d:2d:14:17"
 
 input_file = 'measurments/dinsdagwoensdagfixed.csv'
 output_file = 'dinsdagwoensdag.csv'
@@ -28,7 +34,7 @@ class GuiTracker:
 			Parent = self.tree.insert("", "end", text=MAC, values=(len(MACtionary[MAC]), MACtionary[MAC][-1]))
 			if len(MACtionary[MAC]) > 1:
 				for Time in MACtionary[MAC]:
-					self.tree.insert(Parent, "end", MAC + str(Time), text=" " + MAC , values=(" ", Time))
+					self.tree.insert(Parent, "end", MAC + str(Time), text=" " + MAC , values=(" ", datetime.datetime.fromtimestamp(int(Time)).strftime('%Y-%m-%d %H:%M:%S')))
 		self.myContainer.pack()
 
 		self.tree.pack(side="left")
@@ -45,22 +51,23 @@ readlines = 0
 with open(input_file, newline='') as csvfile:
 	spamreader = csv.reader(csvfile, delimiter='\t')
 	for row in spamreader:
-		readlines = readlines + 1
-		if not row[1] in macaskey:
-			macaskey.update({row[1]:[]})
+		if row[1].endswith(starts_with):
+			readlines = readlines + 1
+			if not row[1] in macaskey:
+				macaskey.update({row[1]:[]})
 
-		if (len(macaskey[row[1]])) > 1:
-			if ((float(row[0]) - macaskey[row[1]][-1]) < declutter_min) and (macaskey[row[1]][-1] - macaskey[row[1]][-2] < declutter_max):
-				macaskey[row[1]][-1] = float(row[0])
+			if (len(macaskey[row[1]])) > 1:
+				if ((float(row[0]) - macaskey[row[1]][-1]) < declutter_min) and (macaskey[row[1]][-1] - macaskey[row[1]][-2] < declutter_max):
+					macaskey[row[1]][-1] = float(row[0])
+				else:
+					macaskey[row[1]].append(float(row[0]))
 			else:
 				macaskey[row[1]].append(float(row[0]))
-		else:
-			macaskey[row[1]].append(float(row[0]))
 
 macstoremove= []
 for MAC in macaskey:
 
-	if len(macaskey[MAC]) == 2 and ((macaskey[MAC][1] - macaskey[MAC][0]) < declutter_min): # and Ignore_Singletons:
+	if len(macaskey[MAC]) == 2 and ((macaskey[MAC][1] - macaskey[MAC][0]) < declutter_min):  # and Ignore_Singletons:
 		macaskey[MAC].pop()
 	if len(macaskey[MAC])== 1 and Ignore_Singletons:
 		macstoremove.append(MAC)
